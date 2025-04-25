7e6236d13ced2b540b739bc8c4bdaf769bead07b
Juan Manuel Rico <juanmard@gmail.com>
2025-04-25 03:38:05 +0200

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -121,7 +121,7 @@ gruntCfg.nwjs = {
     version: nwjsVersion,
     buildDir: 'dist/',
     platforms: platforms,
-    flavor: 'sdk',
+    flavor: 'stable',
     zip: false,
     winIco: 'docs/_static/img/logo/icestudio-logo.ico',
     macIcns: 'docs/_static/img/logo/nw.icns',
